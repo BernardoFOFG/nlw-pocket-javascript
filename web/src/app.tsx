b7d8b6762cd61a7cf2119c94720ad11c812b8a38
BernardoFofg-NE <bernardofofg@gmail.com>
2024-09-14 14:28:30 -0300

    feat: first screen the application

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,3 +1,24 @@
+import logo from "../src/assets/logo-in-orbit.svg";
+import letsStart from "../src/assets/lets-start-illustration.svg";
+import { Plus } from "lucide-react";
+
 export const App = () => {
-  return <h1 className="text-red-500">Hello world</h1>;
+  return (
+    <div className="h-screen flex items-center flex-col justify-center gap-8">
+      <img src={logo} alt="in.orbit" />
+      <img src={letsStart} alt="in.orbit" />
+      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+        Voce ainda não cadastrou nenhuma meta, que tal cadastrar uma agora
+        mesmo?
+      </p>
+
+      <button
+        type="button"
+        className="px-4 py-2.5 rounded-lg bg-violet-500 text-violet-50 flex items-center gap-2 text-sm font-medium tracking-tight hover:bg-violet-600 transition-all"
+      >
+        <Plus className="size-4" />
+        Cadastrar meta
+      </button>
+    </div>
+  );
 };
