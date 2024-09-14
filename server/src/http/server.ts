eb109c20f52c290381dbca110a071e2e871c7cde
BernardoFofg-NE <bernardofofg@gmail.com>
2024-09-14 00:26:46 -0300

    feat: add get week summary

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -6,8 +6,9 @@ import {
 } from "fastify-type-provider-zod";
 import { getWeekPendingGoals } from "../functions/get-week-pending-goals";
 import { createGoalRoute } from "./routes/create-goals";
-import { createCompletionRoutes } from "./routes/create-completion";
-import { getPendingGoalsRoutes } from "./routes/get-pending-goals";
+import { createCompletionRoute} from "./routes/create-completion";
+import { getPendingGoalsRoute } from "./routes/get-pending-goals";
+import { getWeekSummaryRoute } from "./routes/get-week-summary";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>(); // Essas linhas servem para passar habilitar o plugin do zod validar as informações passadas no corpo da requisição
 app.setValidatorCompiler(validatorCompiler);
@@ -15,8 +16,9 @@ app.setSerializerCompiler(serializerCompiler);
 
 // Criando as rotas como plugin do fastify
 app.register(createGoalRoute);
-app.register(createCompletionRoutes);
-app.register(getPendingGoalsRoutes);
+app.register(createCompletionRoute);
+app.register(getPendingGoalsRoute);
+app.register(getWeekSummaryRoute);
 
 app
   .listen({
