27185f65f37ef087912bf9bff42d2a7839a8933b
BernardoFofg-NE <bernardofofg@gmail.com>
2024-09-16 11:07:51 -0300

    feat: form create goals

--- web/src/components/create-goal.tsx
+++ web/src/components/create-goal.tsx
@@ -14,8 +14,37 @@ import {
 import { desiredWeeklyList } from "../utils";
 import { Button } from "./ui/button";
 import { X } from "lucide-react";
+import { Controller, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createGoal } from "../http/create-goal";
+import { useQueryClient } from "@tanstack/react-query";
+
+const createGoalForm = z.object({
+  title: z.string().min(1, "Informe a atividade que deseja realizar."),
+  desiredWeeklyFrequency: z.coerce.number().min(1).max(7),
+});
+
+type CreateGoalForm = z.infer<typeof createGoalForm>;
 
 export const CreateGoal = () => {
+  const queryClient = useQueryClient();
+  const { register, control, handleSubmit, formState, reset } =
+    useForm<CreateGoalForm>({
+      resolver: zodResolver(createGoalForm),
+    });
+
+  const handleCreateGoal = async (data: CreateGoalForm) => {
+    await createGoal({
+      title: data.title,
+      desiredWeeklyFrequency: data.desiredWeeklyFrequency,
+    });
+
+    queryClient.invalidateQueries({ queryKey: ["summary"] });
+    queryClient.invalidateQueries({ queryKey: ["pending-goals"] });
+
+    reset();
+  };
   return (
     <DialogContent>
       <div className="flex flex-col gap-6 h-full">
@@ -32,7 +61,10 @@ export const CreateGoal = () => {
           </DialogDescription>
         </div>
 
-        <form action="" className="flex flex-col justify-between flex-1">
+        <form
+          onSubmit={handleSubmit(handleCreateGoal)}
+          className="flex flex-col justify-between flex-1"
+        >
           <div className="flex flex-col gap-6">
             <div className="flex flex-col gap-2">
               <Label htmlFor="title">Qual é a atividade?</Label>
@@ -40,24 +72,43 @@ export const CreateGoal = () => {
                 id="title"
                 autoFocus
                 placeholder="Praticar exercícios, meditar e etcs"
+                {...register("title")}
               />
+              {formState.errors.title && (
+                <p className="text-red-400 text-sm">
+                  {formState.errors.title.message}
+                </p>
+              )}
             </div>
 
             <div className="flex flex-col gap-2">
               <Label>Quantas vezes na semana?</Label>
-              <RadioGroup>
-                {desiredWeeklyList.map((v) => (
-                  <RadioGroupItem value={v.value} key={v.value}>
-                    <RadioGroupIndicator />
-                    <span className="text-zinc-300 text-sm font-meidum leading-none">
-                      {v.value === "7"
-                        ? "Todos os dias na semana"
-                        : `${v.value}x na semana"`}
-                    </span>
-                    <span className="text-lg leading-none">{v.emoji}</span>
-                  </RadioGroupItem>
-                ))}
-              </RadioGroup>
+              <Controller
+                control={control}
+                name="desiredWeeklyFrequency"
+                render={({ field }) => {
+                  return (
+                    <RadioGroup
+                      onValueChange={field.onChange}
+                      value={String(field.value)}
+                    >
+                      {desiredWeeklyList.map((v) => (
+                        <RadioGroupItem value={v.value} key={v.value}>
+                          <RadioGroupIndicator />
+                          <span className="text-zinc-300 text-sm font-meidum leading-none">
+                            {v.value === "7"
+                              ? "Todos os dias na semana"
+                              : `${v.value}x na semana"`}
+                          </span>
+                          <span className="text-lg leading-none">
+                            {v.emoji}
+                          </span>
+                        </RadioGroupItem>
+                      ))}
+                    </RadioGroup>
+                  );
+                }}
+              />
             </div>
           </div>
 
