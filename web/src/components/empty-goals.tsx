73edb97c4a5eb5d5469fad39de8fa5c36bae2970
BernardoFofg-NE <bernardofofg@gmail.com>
2024-09-17 17:54:22 -0300

    fix: front-end

--- web/src/components/empty-goals.tsx
+++ web/src/components/empty-goals.tsx
@@ -1,24 +1,31 @@
-import logo from "../assets/logo-in-orbit.svg";
-import letsStart from "../assets/lets-start-illustration.svg";
-import { DialogTrigger } from "./ui/dialog";
-import { Button } from "./ui/button";
 import { Plus } from "lucide-react";
+import { DialogTrigger } from "@radix-ui/react-dialog";
 
-export const EmptyGoals = () => {
+import logo from "../assets/logo-in-orbit.svg";
+import rocketLaunchIllustration from "../assets/lets-start-illustration.svg";
+import { Button } from "./ui/button";
+
+export function EmptyGoals() {
   return (
-    <div className="h-screen flex items-center flex-col justify-center gap-8">
+    <main className="h-screen flex flex-col items-center justify-center gap-8">
       <img src={logo} alt="in.orbit" />
-      <img src={letsStart} alt="in.orbit" />
+
+      <img
+        src={rocketLaunchIllustration}
+        alt="Ilustração de uma mulher controlando um lançamento de um foguete através de um controle remoto"
+      />
+
       <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
-        Voce ainda não cadastrou nenhuma meta, que tal cadastrar uma agora
+        Você ainda não cadastrou nenhuma meta, que tal cadastrar uma agora
         mesmo?
       </p>
+
       <DialogTrigger asChild>
         <Button>
           <Plus className="size-4" />
           Cadastrar meta
         </Button>
       </DialogTrigger>
-    </div>
+    </main>
   );
-};
+}
